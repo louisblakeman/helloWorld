8468529648e4dc33bf7bf64c4518290adfa95869
louisblakeman <louisb@terpmail.umd.edu>
2023-02-06 19:48:51 -0500

    I am adding my first HTML page.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World from Louis Blakeman! I am adding my first code change.'
+@app.route('/hello')
+def hello():  # put application's code here
+    return render_template('hello.html')
+
 
 
 if __name__ == '__main__':
