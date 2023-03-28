d15278266ece5c2e4d288ee8bb2c9fe2f054cedb
louisblakeman <louisb@terpmail.umd.edu>
2023-03-28 19:45:31 -0400

    Added 3 new templates (base.html,contact.html, and favorite-course.html)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,16 @@ def hello():  # put application's code here
 def about():  # put application's code here
     return render_template('about.html')
 
+@app.route('/favorite-course')
+def favorite_course():  # put application's code here
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():  # put application's code here
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
